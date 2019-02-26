56ace498595b153879ffe64713e8008797ee76c6
Andrea Johnson <ajohnson1@ajohnson-kcmo.vml.lan>
2019-02-26 12:06:29 -0600

    Add tsconfig file; created a Scone class; this doesn't work and I get an error message

--- Scripts/main.js
+++ Scripts/main.js
@@ -7,24 +7,21 @@ class Muffin {
         this.flavorType = flavor;
     }
 }  */
-
-//this, for some reason, is not working; error in browser: "TypeError: Scone is not a constructor"
-class Scone {
-    flavor: string; //variable
-    constructor(kind: string) { 
+var Scone = /** @class */ (function () {
+    function Scone(kind) {
         this.flavor = kind;
     } //for when it's initialized 
-    bake() { 
+    Scone.prototype.bake = function () {
         alert("You started baking " + this.flavor + " scones.");
-    } //function
-    eat() {
+    }; //function
+    Scone.prototype.eat = function () {
         alert("Time to eat some " + this.flavor + " scones.");
-    }
-}
-
+    };
+    return Scone;
+}());
 //add window on load capability
-window.onload = function() {
+window.onload = function () {
     var Scone = new Scone('orange cranberry');
     Scone.bake();
     Scone.eat();
-}
+};
