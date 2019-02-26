d5a8a8145d80087962571e491337039fc30162bb
Andrea Johnson <ajohnson1@ajohnson-kcmo.vml.lan>
2019-02-26 10:51:18 -0600

    First commit

---
+++ Scripts/main.ts
@@ -0,0 +1,16 @@
+/**
+ * Just a place to practice TS from the PluralSight course
+
+class Muffin {
+    flavorType: string;
+    constructor(flavor: string) {
+        this.flavorType = flavor;
+    }
+}  */
+
+class Scone {
+    flavor: string;
+    constructor(flavor: string) {
+        this.flavor = flavor;
+    }
+}
